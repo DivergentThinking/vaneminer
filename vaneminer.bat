b648285a86a9bc2926d9a1eabd12a998ba554997
tphecca <tphecca@gmail.com>
2017-08-09 22:12:21 -0500

    Fix example

--- vaneminer.bat
+++ vaneminer.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p pubkey="Enter your Snatcoin address: "
 set /p usegpu="Should I use your graphics card to mine? Type either 'true' or 'false': "
-set /p VANITYGEN_OPTIONS="Any options (like GPU device, '-D 2') for Vanitygen? Hit enter for none: "
+set /p VANITYGEN_OPTIONS="Any options (like GPU device, '-D 1:1') for Vanitygen? Hit enter for none: "
 node index.js %pubkey% -g %usegpu%
 pause
